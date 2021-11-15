f1cf65f66f901bb3e24dcc1a54394ec1e16f6f0a
Your Name <you@example.com>
2021-11-15 15:57:58 +0800

    update

--- main/pages/cover.js
+++ main/pages/cover.js
@@ -1,17 +1,31 @@
-import React from "react"
-import { fetchAPI } from "../lib/api"
-import Link from "next/link"
-import Image from "next/image"
+import * as React from "react"
+import Card from "@mui/material/Card"
+import CardContent from "@mui/material/CardContent"
+import CardMedia from "@mui/material/CardMedia"
+import Typography from "@mui/material/Typography"
+import { CardActionArea } from "@mui/material"
 
-const Home = ({ articles, categories, homepage }) => {
+export default function ActionAreaCard() {
   return (
-    <div>
-      <div>the next novel</div>
-      <p className="intro">
-        Hello, I am Lai shu Lap. This is my blog. In here you can find
-        <div>Reveal</div>
-      </p>
-    </div>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   )
 }
 
