ecef8ed5b90770b07d2ac738749640bb652da1e2
Your Name <you@example.com>
2021-11-18 23:22:45 +0800

    update

--- components/content.js
+++ components/content.js
@@ -1,31 +1,51 @@
 import React, { useEffect, useState } from "react"
 
-import NextButtons from "../components/button.js"
-import RevealButtons from "../components/buttonreveal.js"
 
-
-export default function Content({article}) {
+export default function Content({ article }) {
   console.log(article)
   const [random, setRandom] = useState("")
+    const [reveal, setReveal] = useState("")
+
   useEffect(() => {
     randomItem()
   }, [])
   const randomItem = () => {
     let randomNum = Math.floor(Math.random() * article.length)
     let randomElement = article[randomNum]
-    setRandom(randomElement.description)
+    setRandom(randomElement.description)     
+    setReveal(randomElement.title)
+
     console.log(randomElement.description)
   }
   const handleClick = () => {
     randomItem()
   }
+  
   return (
     <div>
-     <NextButtons />
+      <div class="flex my-16	 justify-center items-center">
+        <button
+          onClick={handleClick}
+          class="flex  content-center justify-center justify-self-auto bg-gray-300 hover:bg-gray-600 text-gray-800 font-semibold py-12 px-16 border border-gray-400 rounded shadow"
+        >
+          Next
+        </button>
+      </div>
 
       <div>
         <p>{random}</p>
       </div>
-      <RevealButtons></RevealButtons></div>
+      <div class="my-16	flex h-screen justify-center items-center">
+        <button
+          
+          class="flex  content-center justify-center justify-self-auto bg-gray-300 hover:bg-gray-600 text-gray-800 font-semibold py-12 px-16 border border-gray-400 rounded shadow"
+        >
+          Reveal
+        </button>
+        <div>
+          <p>{reveal}</p>
+        </div>
+      </div>
+    </div>
   )
 }
