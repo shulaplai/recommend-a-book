440ff2d9e6d79eddb8d4f0111ffe5dfac3f02944
Your Name <you@example.com>
2021-11-18 17:32:37 +0800

    update

--- main/components/nav/SidebarData.js
+++ main/components/nav/SidebarData.js
@@ -1,23 +1,19 @@
 import React from "react"
 
-import { HiDesktopComputer } from "react-icons/hi"
 export const SidebarData = [
   {
     title: "Read a Page",
     path: "./",
-    icon: <HiDesktopComputer />,
     cName: "nav-text",
   },
   {
     title: "Cover",
     path: "./cover",
-    icon: <HiDesktopComputer />,
     cName: "nav-text",
   },
   {
     title: "Submit",
     path: "./addArticle",
-    icon: <HiDesktopComputer />,
     cName: "nav-text",
   },
 ]
